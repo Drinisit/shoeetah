0550f3522117109f1a830b0c61831b425bd32e9f
Drin <aldrinisit3196@gmail.com>
2024-02-09 22:52:11 +0800

    Fix: style on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ function Navbar() {
     <>
             <header className={style.header}>
                 <h1 className={style.h1}>Shoeeta</h1>
-
                  
                         <nav className={style.nav}>
                             <ul className={style.ul}>
@@ -18,21 +17,21 @@ function Navbar() {
                                
                             </ul>
 
+                            <div className={style.access}>
+                                <div className={style.search_input}>
+                                  <input type='text' placeholder='Search...'></input>
+                                  <CiSearch className={style.search_icon} />
+                                </div>
+                              
+                                  <CiShoppingCart className={style.cart_icon} />
+                            
+                                  <CiUser className={style.user_icon} />
+                            </div>
                             
                         </nav>
 
                       
-                       <div className={style.access}>
-                          <div className={style.search_input}>
-                            <input type='text' placeholder='Search...'></input>
-                            <CiSearch className={style.search_icon} />
-                          </div>
-                         
-                             <CiShoppingCart className={style.cart_icon} />
-                          
-                          
-                          <CiUser className={style.user_icon} />
-                       </div>
+                       
                
             </header>
            
