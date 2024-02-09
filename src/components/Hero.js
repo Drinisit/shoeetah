dabf7a55f9aaf98b765a89caebd45f459215de6e
Drin <aldrinisit3196@gmail.com>
2024-02-09 23:12:01 +0800

    Add: Hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from './Hero.module.css'
+
+function Hero() {
+  return (
+    <>
+    <div className={style.hero}>
+        <h1>Hero</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium sint velit fuga, nostrum blanditiis nam repudiandae repellat, quidem quae dolorem quibusdam voluptas modi deserunt aliquam eveniet commodi. Vero, maxime omnis.</p>
+    </div>
+   
+    
+    </>
+  )
+}
+
+export default Hero
