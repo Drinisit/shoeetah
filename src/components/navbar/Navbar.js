43045d71bd207ca4e55714b0a43a1b839da2da0e
Drin <aldrinisit3196@gmail.com>
2024-02-15 06:18:32 +0800

    Style: fix mobile nav btn positioning

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Navbar.module.css'
-import { CiSearch, CiShoppingCart} from "react-icons/ci";
+import { CiSearch} from "react-icons/ci";
+import { RiUserFill } from "react-icons/ri";
+import { PiShoppingCartSimpleFill } from "react-icons/pi";
 import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
 
 function Navbar() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
   return (
     <>
         <header className={styles.header}>
@@ -19,16 +24,32 @@ function Navbar() {
                     <li>New Arrival</li>
                 </ul>
 
-                <div className={styles.search}>
-                  <input className={styles.searchBar} type="text" placeholder='Search' />
-                      <CiSearch className={styles.search_icon}/>
-                </div>
-                    
-                      <CiShoppingCart className={styles.cart_icon} />
-                      
-                      <IoMenuOutline className={styles.mobile_btn} />
-            </nav>
+                <div className={styles.users_access}>
+                  <div className={styles.search}>
+                    <input className={styles.searchBar} type="text" placeholder='Search' />
+                        <CiSearch className={styles.search_icon}/>
+                  </div>
 
+                  <PiShoppingCartSimpleFill className={styles.cart_icon} />
+                  <RiUserFill className={styles.user_icon} />
+
+                  <div onClick={() => setIsMobile(!isMobile)}>
+           
+                        {
+                          isMobile ? [<IoCloseOutline className={styles.mobile_closebtn} />] : [<IoMenuOutline className={styles.mobile_btn} />]
+                        }
+                      
+                  </div>
+
+                </div>
+                
+                    
+                      
+                      
+            </nav>
+            
+
+                      
         </header>
     </>
   )
