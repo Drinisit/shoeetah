92456c6a59be92ab968c4a881526dbef8665a198
Drin <aldrinisit3196@gmail.com>
2024-02-17 21:07:50 +0800

    Add: style on hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,21 @@
+import styles from './Hero.module.css'
+import HeroShoes from '../../assets/vans_hero.png'
+
+function hero() {
+  return (
+    <div className={styles.hero}>
+        <div className={styles.content}>
+            <div className={styles.text}>
+                <h1 className={styles.title}>Stride with Style</h1>
+                <h1 className={styles.title}>Find your Perfect Pair</h1>
+            </div>
+            <div >
+                <img className={styles.hero_image} src={HeroShoes} alt='' />
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default hero
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import Navbar from '../components/navbar/Navbar'
+import Hero from '../components/hero/Hero'
 function Home() {
   return (
     <>
         <Navbar />
+        <Hero />
     </>
   )
 }
