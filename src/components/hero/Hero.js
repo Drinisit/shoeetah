4b7683e2063f6728708597151d7e5efcd2a3c960
Drin <aldrinisit3196@gmail.com>
2024-02-18 15:28:56 +0800

    Fix: hero style and design for mobile and big screen

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,6 +8,7 @@ function hero() {
             <div className={styles.text}>
                 <h1 className={styles.title}>Stride with Style</h1>
                 <h1 className={styles.title}>Find your Perfect Pair</h1>
+                <button className={styles.btn}>Learn more</button>
             </div>
             <div >
                 <img className={styles.hero_image} src={HeroShoes} alt='' />
