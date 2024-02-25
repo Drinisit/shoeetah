6613e4a32679d36635ac90745c9751fb12a65fb6
Drin <aldrinisit3196@gmail.com>
2024-02-25 16:52:14 +0800

    Fix: style display single card below 1024 and 3 cards over 1025

--- src/components/review/Review.js
+++ src/components/review/Review.js
@@ -1,8 +1,12 @@
 import styles from "./Review.module.css"
 import REVIEWS from "./ReviewList"
+
+import { Pagination, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 
 function Review() {
@@ -11,13 +15,23 @@ function Review() {
         <div className={styles.content}>
 
             <Swiper
-                spaceBetween={50}
+                modules={[Pagination, Navigation]}
+                spaceBetween={20}
                 slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                breakpoints={{
+                 
+                  1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+                  },
+                }}
                 >
                   {
                         REVIEWS.map((reviewer) => {
                             return(    
-                <SwiperSlide key={reviewer.id} className={styles.card}>
+                         <SwiperSlide key={reviewer.id} className={styles.card}>
                                     
                                     <img className={styles.image} src={reviewer.image} alt="" />
                                     <div className={styles.name}>{reviewer.name}</div>
@@ -25,7 +39,7 @@ function Review() {
                               
                                
                     
-                </SwiperSlide>
+                      </SwiperSlide>
 
 )
 })
