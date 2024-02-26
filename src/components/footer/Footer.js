e99599e2cb2eff4f883c7e028f04d96ca62df3c8
Drin <aldrinisit3196@gmail.com>
2024-02-26 22:34:03 +0800

    Add: section on footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,21 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
     <div className={styles.footer}>
-        <div className={styles.content}></div>
+        <div className={styles.content}>
+            <div className={styles.section}>
+                <div>
+                <h1 className={styles.logo}>Shoeetah</h1>
+                </div>
+            
+            </div>
+            <div className={styles.section}>
+                 
+            </div>
+            <div className={styles.section}>
+                 
+            </div>
+            
+        </div>
     </div>
   )
 }
