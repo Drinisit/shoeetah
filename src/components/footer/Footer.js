df6893e2f1c9d41142bceef4cfcc81ed26410d94
Drin <aldrinisit3196@gmail.com>
2024-02-26 22:44:34 +0800

    Add: menu and logo on footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,13 +5,22 @@ function Footer() {
     <div className={styles.footer}>
         <div className={styles.content}>
             <div className={styles.section}>
-                <div>
+                <div className={styles.about}>
                 <h1 className={styles.logo}>Shoeetah</h1>
+                
                 </div>
-            
+                <p>This website is for practice only. All image are credit to the owners.</p>
             </div>
             <div className={styles.section}>
-                 
+                 <div className={styles.menu}>
+                        <ul>
+                            <li>Men</li>
+                            <li>Women</li>
+                            <li>Kids</li>
+                            <li>Trends</li>
+                            <li>New Arrival</li>
+                        </ul>
+                 </div>
             </div>
             <div className={styles.section}>
                  
